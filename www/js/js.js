1587d4e33bf6b62d7a3318a941f58917223f27f4
Olivier Bonnaure <o.bonnaure@solisoft.net>
2013-06-21 09:54:47 +0200

    bootstrap

--- www/js/js.js
+++ www/js/js.js
@@ -1,15 +1,5 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
-function getCoords() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    document.getElementById("pos").innerHTML = document.getElementById("pos").innerHTML + "<br/>" + Date() + " : " + position.coords.latitude + "," +position.coords.longitude;
-  });
-}
-
 function onDeviceReady() {
-  var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
-  setInterval(function() {
-    getCoords();
-  }, 60000);
-  getCoords();
+
 }

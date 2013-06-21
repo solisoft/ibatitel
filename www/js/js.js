b4feb9b604bdd7f7555fba65bf1b910f50b6b3ef
Olivier Bonnaure <o.bonnaure@solisoft.net>
2013-06-21 13:14:31 +0200

    config

--- www/js/js.js
+++ www/js/js.js
@@ -1,5 +1,10 @@
 document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("oncreate", onCreate, false);
 
+
+function onCreate() {
+  //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+}
 function onDeviceReady() {
 
 }

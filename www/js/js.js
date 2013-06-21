169efff1bc271a58fda3ab0c0d6c22d441d898a9
Olivier Bonnaure <o.bonnaure@solisoft.net>
2013-06-21 09:29:14 +0200

    Skeleton - test DB

--- www/js/js.js
+++ www/js/js.js
@@ -1,14 +1,13 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
-// Cordova is ready
-//
-
 function getCoords() {
   navigator.geolocation.getCurrentPosition(function(position) {
     document.getElementById("pos").innerHTML = document.getElementById("pos").innerHTML + "<br/>" + Date() + " : " + position.coords.latitude + "," +position.coords.longitude;
   });
 }
+
 function onDeviceReady() {
+  var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
   setInterval(function() {
     getCoords();
   }, 60000);
